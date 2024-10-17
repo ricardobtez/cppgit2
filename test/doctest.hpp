d5d43ae63061db408be734c637a9cc2799b47a24
Ricardo Benitez <benitezc.ricardo@gmail.com>
2024-10-17 21:24:33 +0200

    style: update style of doctest
    Signed-off-by: Ricardo Benitez <benitezc.ricardo@gmail.com>

--- test/doctest.hpp
+++ test/doctest.hpp
@@ -7812,8 +7812,8 @@ int Context::run() {
 
 #ifndef DOCTEST_CONFIG_NO_EXCEPTIONS
         try {
-#endif  // DOCTEST_CONFIG_NO_EXCEPTIONS
-          // MSVC 2015 diagnoses fatalConditionHandler as unused (because reset() is a static method)
+#endif  // DOCTEST_CONFIG_NO_EXCEPTIONS \
+    // MSVC 2015 diagnoses fatalConditionHandler as unused (because reset() is a static method)
           DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(
               4101)  // unreferenced local variable
           FatalConditionHandler fatalConditionHandler;  // Handle signals
